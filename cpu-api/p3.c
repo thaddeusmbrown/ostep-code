95d84a6d5edf64c2089b4338d8bcf6d215dcc17c
thaddeusmbrown <33402458+thaddeusmbrown@users.noreply.github.com>
2023-02-23 18:28:32 -0600

    Complete 5 Process API section

--- cpu-api/p3.c
+++ cpu-api/p3.c
@@ -4,17 +4,13 @@
 #include <string.h>
 #include <sys/wait.h>
 
-int
-main(int argc, char *argv[])
-{
-    printf("hello world (pid:%d)\n", (int) getpid());
+int main(int argc, char *argv[]) {
+    printf("hello, world (pid:%d)\n", (int) getpid());
     int rc = fork();
-    if (rc < 0) {
-        // fork failed; exit
+    if (rc < 0) {           // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
-    } else if (rc == 0) {
-        // child (new process)
+    } else if (rc == 0) {   // child (new process)
         printf("hello, I am child (pid:%d)\n", (int) getpid());
         char *myargs[3];
         myargs[0] = strdup("wc");   // program: "wc" (word count)
@@ -23,10 +19,9 @@ main(int argc, char *argv[])
         execvp(myargs[0], myargs);  // runs word count
         printf("this shouldn't print out");
     } else {
-        // parent goes down this path (original process)
-        int wc = wait(NULL);
-        printf("hello, I am parent of %d (wc:%d) (pid:%d)\n",
-	       rc, wc, (int) getpid());
+        int rc_wait = wait(NULL);
+        printf("hello, I am parent of %d (rc_wait:%d) (pid%d)\n",
+                rc, rc_wait, (int) getpid());
     }
     return 0;
 }
--- cpu-api/p4.c
+++ cpu-api/p4.c
@@ -3,32 +3,29 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-#include <assert.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int rc = fork();
     if (rc < 0) {
-        // fork failed; exit
+        // fork failed
         fprintf(stderr, "fork failed\n");
         exit(1);
-    } else if (rc == 0) {
-	// child: redirect standard output to a file
-	close(STDOUT_FILENO); 
-	open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+    } else if(rc == 0) {
+        // child: redirect standard output to a file
+        close(STDOUT_FILENO);
+        open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
 
-	// now exec "wc"...
+        // now exec "wc"...
         char *myargs[3];
-        myargs[0] = strdup("wc");   // program: "wc" (word count)
-        myargs[1] = strdup("p4.c"); // argument: file to count
-        myargs[2] = NULL;           // marks end of array
-        execvp(myargs[0], myargs);  // runs word count
+        myargs[0] = strdup("wc");   // program: wc (word count)
+        myargs[1] = strdup("p4.c"); // arg: file to count
+        myargs[2] = NULL;           // mark end of array
+        execvp(myargs[0], myargs);  //runs word count
     } else {
-        // parent goes down this path (original process)
-        int wc = wait(NULL);
-	assert(wc >= 0);
+        // parent goes down this path (main)
+        int rc_wait = wait(NULL);
     }
     return 0;
 }
